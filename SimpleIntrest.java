a6d2189c4208f1a5148ce4d49b30f92b5146e32c
Jagankumar26 <jaganmarupaka4@gmail.com>
2024-07-18 23:40:34 +0530

    Add files via upload

---
+++ SimpleIntrest.java
@@ -0,0 +1,15 @@
+package anudip;
+
+public class SimpleIntrest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double p=15000;
+int r=2;
+int t=3;
+double total=p+(p*t*r)/100;
+System.out.println(total);
+
+	}
+
+}
